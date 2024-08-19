cf8ba3bc20fde7c03a7b75acac7e8cb2ebe37802
thchoRepo <mythcho88@gmail.com>
2024-08-19 20:40:02 +0900

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -136,6 +136,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -144,7 +148,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 
 init();
