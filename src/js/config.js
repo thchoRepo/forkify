d0582a0c1e1f186c860ee6ae7559a5d92447b7f3
thchoRepo <mythcho88@gmail.com>
2024-08-19 20:23:44 +0900

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '535ab4de-5e99-4746-a41d-af4cf6c0af5f';
+export const MODAL_CLOSE_SEC = 2.5;
